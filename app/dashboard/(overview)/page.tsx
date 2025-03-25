008b5fed09a62cdb6d4fccc1445ff9a9779b7a92
gaksss <clement.carlier2@gmail.com>
2025-03-25 16:18:21 +0100

    DONE: Part12

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -1,3 +1,4 @@
+
 import RevenueChart from "@/app/ui/dashboard/revenue-chart";
 import LatestInvoices from "@/app/ui/dashboard/latest-invoices";
 import { lusitana } from "@/app/ui/fonts";
@@ -5,7 +6,7 @@ import { Suspense } from "react";
 import {
   RevenueChartSkeleton,
   LatestInvoicesSkeleton,
-  CardsSkeleton,
+  CardsSkeleton
 } from "@/app/ui/skeletons";
 import CardWrapper from "@/app/ui/dashboard/cards";
 
@@ -16,16 +17,16 @@ export default async function Page() {
         Dashboard
       </h1>
       <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
-        <Suspense fallback={<CardsSkeleton />}>
-          <CardWrapper />
-        </Suspense>
+       <Suspense fallback={<CardsSkeleton />}>
+       <CardWrapper />
+       </ Suspense>
       </div>
       <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
         <Suspense fallback={<RevenueChartSkeleton />}>
           <RevenueChart />
         </Suspense>
         <Suspense fallback={<LatestInvoicesSkeleton />}>
-          <LatestInvoices />
+        <LatestInvoices />
         </Suspense>
       </div>
     </main>
--- app/ui/dashboard/cards.tsx
+++ app/ui/dashboard/cards.tsx
@@ -58,6 +58,9 @@ export function Card({
       >
         {value}
       </p>
+      
     </div>
   );
 }
+
+
